dbbc92b2d374e304476add4a69b2abac47603bcf
lainz <leandrodiazoe@gmail.com>
2019-11-05 22:43:21 -0300

    Update bgrawebptest.lpi, umain.lfm, and umain.pas

--- bgrawebp/umain.pas
+++ bgrawebp/umain.pas
@@ -6,17 +6,23 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, libwebp,
-  bgrabitmap, bgrabitmaptypes;
+  BGRAVirtualScreen, bgrabitmap, bgrabitmaptypes, BCTypes;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    BGRAVirtualScreen1: TBGRAVirtualScreen;
     Button1: TButton;
+    Button2: TButton;
+    procedure BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
-
+    bgrab: TBGRABitmap;
   public
 
   end;
@@ -49,6 +55,66 @@ begin
   end;
   fileWebp.Free;
   bmp.Free;
+end;
+
+procedure TForm1.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+begin
+  Bitmap.Fill(BGRAWhite);
+  Bitmap.PutImage(0, 0, bgrab, dmDrawWithTransparency);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  i: integer;
+  bmp: TBGRABitmap;
+  fileWebP: TFileStream;
+  inWebP: array of byte;
+  outWebP: PByte;
+  w, h: Integer;
+  p: PBGRAPixel;
+begin
+
+  fileWebP := TFileStream.Create('file.webp', fmOpenRead);
+
+  SetLength(inWebP, fileWebP.Size);
+
+  //ShowMessage(fileWebP.Size.ToString);
+
+  for i:=0 to fileWebP.Size-1 do
+  begin
+    inWebP[i] := fileWebP.ReadByte;
+  end;
+
+  WebPGetInfo(@inWebP[0], fileWebP.Size, @w, @h);
+  outWebP := WebPDecodeBGRA(@inWebP[0], fileWebP.Size, @w, @h);
+
+  bgrab.SetSize(w, h);
+
+  p := bgrab.Data;
+
+  for i:=0 to (w*h)-1 do
+  begin
+    p^ := outWebP^;
+    inc(p);
+    inc(outWebP,4);
+  end;
+
+  bgrab.InvalidateBitmap;
+
+  //ShowMessage(w.ToString + ' ' + h.ToString);
+
+  fileWebP.Free;
+  BGRAVirtualScreen1.DiscardBitmap;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  bgrab := TBGRABitmap.Create(0,0);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  bgrab.Free;
 end;
 
 end.
