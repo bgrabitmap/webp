4cd944742fd687624a9ce3f8f6a0eccccb6818c4
johann <circular@operamail.com>
2019-11-06 15:35:53 +0100

    rgba/bgra order, free memoy

--- bgrawebp/bgrawebp.pas
+++ bgrawebp/bgrawebp.pas
@@ -19,73 +19,58 @@ type
 
 implementation
 
-{$ifdef Windows}
-uses Windows;
-{$endif}
-
 { TBGRABitmapWebPHelper }
 
 procedure TBGRABitmapWebPHelper.LoadFromWebPFile(FileName: string);
 var
-  i: integer;
   fileWebP: TFileStream;
-  inWebP: array of byte;
+  inWebP: packed array of byte;
   outWebP: PByte;
   w, h: integer;
-  p: PBGRAPixel;
 begin
   fileWebP := TFileStream.Create(FileName, fmOpenRead);
 
   SetLength(inWebP, fileWebP.Size);
+  if inWebP<>nil then
+    fileWebP.Read(inWebP[0], length(inWebP));
+  fileWebP.Free;
 
-  for i := 0 to fileWebP.Size - 1 do
-  begin
-    inWebP[i] := fileWebP.ReadByte;
-  end;
+  WebPGetInfo(@inWebP[0], length(inWebP), @w, @h);
 
-  WebPGetInfo(@inWebP[0], fileWebP.Size, @w, @h);
-  outWebP := WebPDecodeRGBA(@inWebP[0], fileWebP.Size, @w, @h);
+  {$PUSH}{$WARNINGS OFF}
+  if TBGRAPixel_RGBAOrder then
+    outWebP := WebPDecodeRGBA(@inWebP[0], length(inWebP), @w, @h)
+  else
+    outWebP := WebPDecodeBGRA(@inWebP[0], length(inWebP), @w, @h);
+  {$POP}
 
   Self.SetSize(w, h);
-
-  p := Self.Data;
-
-  for i := 0 to (w * h) - 1 do
-  begin
-    p^.red := outWebP^;
-    Inc(outWebP);
-    p^.green := outWebP^;
-    Inc(outWebP);
-    p^.blue := outWebP^;
-    Inc(outWebP);
-    p^.alpha := outWebP^;
-    Inc(outWebP);
-    Inc(p);
-  end;
-
-  Self.VerticalFlip;
-
-  fileWebP.Free;
+  move(outWebP^, self.Data^, self.RowSize*h);
+  WebPFree(outWebP);
+  if self.LineOrder = riloBottomToTop then Self.VerticalFlip;
 end;
 
 procedure TBGRABitmapWebPHelper.SaveToWebPFile(FileName: string; Quality: single);
 var
   outWebP: PByte;
   fileWebP: TFileStream;
-  i: integer;
   outSize: Cardinal;
 begin
   if self.LineOrder = riloBottomToTop then Self.VerticalFlip;
 
-  outSize := WebPEncodeBGRA(Self.DataByte, Self.Width, Self.Height, Self.Width *
-    4, Quality, outWebP{%H-});
+  {$PUSH}{$WARNINGS OFF}
+  if TBGRAPixel_RGBAOrder then
+    outSize := WebPEncodeRGBA(Self.DataByte, Self.Width, Self.Height, Self.Width *
+      4, Quality, outWebP{%H-})
+  else
+    outSize := WebPEncodeBGRA(Self.DataByte, Self.Width, Self.Height, Self.Width *
+      4, Quality, outWebP{%H-});
+  {$POP}
 
   fileWebP := TFileStream.Create(FileName, fmCreate);
   fileWebP.Write(outWebP^, outSize);
   fileWebp.Free;
-
-  //Free or WebPFree(outWebP);
-
+  WebPFree(outWebP);
   if self.LineOrder = riloBottomToTop then Self.VerticalFlip;
 end;
 
